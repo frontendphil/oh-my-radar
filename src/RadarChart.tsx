0988861396dd5d2fc5e56d37c7b52f48bf176749
Philipp Giese <giese.philipp+git@gmail.com>
2022-03-23 22:13:06 +0100

    feat: render dimensions

--- src/RadarChart.tsx
+++ src/RadarChart.tsx
@@ -6,13 +6,27 @@ type Props = {
   scale: number[]
 }
 
-export const RadarChart = ({ title, dimensions, scale }: Props) => (
-  <svg role="figure" aria-label={title}>
-    {dimensions.map((dimension) => (
-      <Dimension key={dimension} title={dimension} scale={scale} />
-    ))}
-  </svg>
-)
+export const RadarChart = ({ title, dimensions, scale }: Props) => {
+  const angle = 360 / dimensions.length
+  const angleOffset = angle / 2
+
+  const width = 500
+  const height = 500
+
+  return (
+    <svg role="figure" aria-label={title} width={width} height={height}>
+      {dimensions.map((dimension, index) => (
+        <g
+          key={dimension}
+          className="origin-center"
+          transform={`rotate(${-90 + angleOffset + angle * index})`}
+        >
+          <Dimension title={dimension} scale={scale} />
+        </g>
+      ))}
+    </svg>
+  )
+}
 
 type DimensionProps = {
   title: string
@@ -22,17 +36,27 @@ type DimensionProps = {
 const Dimension = ({ scale, title }: DimensionProps) => {
   const [selectedValue, setSelectedValue] = useState<number | null>(null)
 
+  const stepSize = 50 / scale.length
+
   return (
     <>
-      <path role="radiogroup" aria-label={title} />
-      {scale.map((value) => (
+      <line
+        role="radiogroup"
+        aria-label={title}
+        x1="50%"
+        y1="50%"
+        x2="100%"
+        y2="50%"
+        className="stroke-slate-700"
+      />
+      {scale.map((value, index) => (
         <circle
           key={value}
           role="radio"
           aria-label={`${title} - ${value}`}
           aria-checked={selectedValue === value}
-          cx={5}
-          cy={5}
+          cx={`${50 + (index + 1) * stepSize}%`}
+          cy="50%"
           r={5}
           className="fill-slate-700"
           onClick={() => setSelectedValue(value)}
