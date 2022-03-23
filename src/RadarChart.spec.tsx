f05a9147f83985b3799a70cd333729f1f8cea51d
Philipp Giese <giese.philipp+git@gmail.com>
2022-03-23 20:03:27 +0100

    feat: render scales

--- src/RadarChart.spec.tsx
+++ src/RadarChart.spec.tsx
@@ -3,8 +3,16 @@ import { RadarChart } from "./RadarChart"
 
 describe("RadarChart", () => {
   it("renders a chat with a title.", () => {
-    render(<RadarChart title="Test" />)
+    render(<RadarChart title="Test" dimensions={[]} scale={[]} />)
 
     expect(screen.getByRole("figure", { name: "Test" })).toBeInTheDocument()
+  })
+
+  it("is possible to select a value for a given dimension.", () => {
+    render(<RadarChart title="Test" dimensions={["Height"]} scale={[1, 2]} />)
+
+    expect(
+      screen.getByRole("checkbox", { name: `Height - 1` })
+    ).toBeInTheDocument()
   })
 })
--- src/RadarChart.tsx
+++ src/RadarChart.tsx
@@ -1,7 +1,26 @@
 type Props = {
   title: string
+  dimensions: string[]
+  scale: number[]
 }
 
-export const RadarChart = ({ title }: Props) => (
-  <svg role="figure" aria-label={title} />
+export const RadarChart = ({ title, dimensions, scale }: Props) => (
+  <svg role="figure" aria-label={title}>
+    {dimensions.map((dimension) => (
+      <Dimension key={dimension} title={dimension} scale={scale} />
+    ))}
+  </svg>
+)
+
+type DimensionProps = {
+  title: string
+  scale: number[]
+}
+
+const Dimension = ({ scale, title }: DimensionProps) => (
+  <>
+    {scale.map((value) => (
+      <circle key={value} role="checkbox" aria-label={`${title} - ${value}`} />
+    ))}
+  </>
 )
