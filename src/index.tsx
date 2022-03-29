c7a25f989a467befbeeda3488d48d44ef4379c99
Philipp Giese <187786+frontendphil@users.noreply.github.com>
2022-03-29 20:43:41 +0200

    chore: migrate to createRoot

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,17 @@
-import { render } from "react-dom"
+import invariant from "invariant"
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
 import { App } from "./App"
 
 import "./main.css"
 
-render(<App />, document.getElementById("app"))
+const container = document.getElementById("app")
+
+invariant(container, 'Could not find app container with id "app".')
+
+const root = createRoot(container)
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
