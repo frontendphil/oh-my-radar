0b5d5d3833ecdb21f0c783a63e735f90bb8d174d
Philipp Giese <187786+frontendphil@users.noreply.github.com>
2022-03-30 21:15:54 +0200

    chore: styles for the color select

--- src/form-controls/ColorSelect.tsx
+++ src/form-controls/ColorSelect.tsx
@@ -5,6 +5,7 @@ import {
   ListboxOption,
   ListboxPopover,
 } from "@reach/listbox"
+import "@reach/listbox/styles.css"
 import { VisuallyHidden } from "@reach/visually-hidden"
 import { HTMLAttributes } from "react"
 import { ColorNames, Colors } from "../configuration"
@@ -22,24 +23,42 @@ export const ColorSelect = ({ label, value, onChange }: Props) => {
     <div>
       <VisuallyHidden id={id}>{label}</VisuallyHidden>
       <ListboxInput aria-labelledby={id} value={value} onChange={onChange}>
-        <ListboxButton aria-label={label}>
+        <ListboxButton
+          aria-label={label}
+          className="flex border-0 p-0 outline-none"
+        >
           <Color color={value} />
         </ListboxButton>
-        <ListboxPopover>
-          <ListboxList>
-            <ListboxOption value={Colors.pink}>
+        <ListboxPopover className="border-1 mt-2 rounded border-gray-200 p-0 shadow-xl focus-within:border-blue-500 focus-within:outline-none focus-within:ring-blue-300">
+          <ListboxList className="flex ">
+            <ListboxOption
+              value={Colors.pink}
+              className="cursor-pointer rounded p-2 hover:bg-pink-50"
+            >
               <Color color={Colors.pink} />
             </ListboxOption>
-            <ListboxOption value={Colors.blue}>
+            <ListboxOption
+              value={Colors.blue}
+              className="cursor-pointer rounded p-2 hover:bg-blue-50"
+            >
               <Color color={Colors.blue} />
             </ListboxOption>
-            <ListboxOption value={Colors.green}>
+            <ListboxOption
+              value={Colors.green}
+              className="cursor-pointer rounded p-2 hover:bg-emerald-50"
+            >
               <Color color={Colors.green} />
             </ListboxOption>
-            <ListboxOption value={Colors.purple}>
+            <ListboxOption
+              value={Colors.purple}
+              className="cursor-pointer rounded p-2 hover:bg-purple-50"
+            >
               <Color color={Colors.purple} />
             </ListboxOption>
-            <ListboxOption value={Colors.yellow}>
+            <ListboxOption
+              value={Colors.yellow}
+              className="cursor-pointer rounded p-2 hover:bg-yellow-100"
+            >
               <Color color={Colors.yellow} />
             </ListboxOption>
           </ListboxList>
