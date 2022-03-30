c804fc586422e3a3fa7f2cf95621a3667a79a57d
Philipp Giese <187786+frontendphil@users.noreply.github.com>
2022-03-30 15:29:40 +0200

    chore: don't swallow props

--- src/radar-chart/Step.tsx
+++ src/radar-chart/Step.tsx
@@ -5,9 +5,8 @@ export const STEP_RADIUS = 5
 type Props = Omit<SVGAttributes<SVGCircleElement>, "cx" | "cy" | "r"> & {
   x: number
   y: number
-  className?: string
 }
 
-export const Step = ({ x, y, className }: Props) => (
-  <circle x={x} y={y} r={STEP_RADIUS} className={className} />
+export const Step = ({ x, y, ...rest }: Props) => (
+  <circle x={x} y={y} r={STEP_RADIUS} {...rest} />
 )
