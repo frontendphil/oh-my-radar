b4017537dcb2d52e5d717de9e8ba29a0dc2dd01d
Philipp Giese <187786+frontendphil@users.noreply.github.com>
2022-03-30 11:06:05 +0200

    test: add selection e2e test

--- cypress/integration/smoke.spec.ts
+++ cypress/integration/smoke.spec.ts
@@ -4,4 +4,16 @@ describe("Smoke test", () => {
 
     cy.findByRole("figure", { name: "Test" }).should("exist")
   })
+
+  it("renders selections correctly.", () => {
+    cy.visit("/")
+
+    cy.findByRole("radio", { name: "One - 1" }).click()
+    cy.findByRole("radio", { name: "Two - 2" }).click()
+    cy.findByRole("radio", { name: "Three - 3" }).click()
+
+    cy.findByRole("figure", { name: "john" }).should("exist")
+
+    cy.percySnapshot()
+  })
 })
