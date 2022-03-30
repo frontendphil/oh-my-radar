5bfa753f305fdc0980c3c160aa92aef0002c708b
Philipp Giese <187786+frontendphil@users.noreply.github.com>
2022-03-30 14:08:05 +0200

    chore: add visual labels to dimensions

--- src/radar-chart/RadarChart.tsx
+++ src/radar-chart/RadarChart.tsx
@@ -1,9 +1,10 @@
-import { ReactNode } from "react"
+import { ReactNode, useLayoutEffect, useRef, useState } from "react"
 
 import { createRange, getDimensionAngle } from "./utils"
 import { Range } from "./types"
 import { RadarContext } from "./RadarContext"
 import { Slots } from "./Slots"
+import { getPoint } from "./getPoint"
 
 type Props<Dimension extends string> = {
   title: string
@@ -24,35 +25,49 @@ export function RadarChart<Dimension extends string>({
 
   return (
     <RadarContext.Provider value={{ diagramWidth: size, dimensions, range }}>
-      <svg role="figure" aria-label={title} width={size} height={size}>
-        <Circles steps={steps} />
+      <div className="relative">
+        <svg
+          className="absolute"
+          role="figure"
+          aria-label={title}
+          width={size}
+          height={size}
+        >
+          <Circles steps={steps} />
 
-        {dimensions.map((dimension, index) => (
-          <g
-            key={dimension}
-            className="origin-center"
-            transform={`rotate(${getDimensionAngle(dimensions, index)})`}
-          >
-            <Dimension title={dimension} diagramWidth={size} />
+          {dimensions.map((dimension, index) => (
+            <g
+              key={dimension}
+              className="origin-center"
+              transform={`rotate(${getDimensionAngle(dimensions, index)})`}
+            >
+              <Dimension title={dimension} diagramWidth={size} />
+            </g>
+          ))}
+
+          <g transform={`translate(${size / 2} ${size / 2})`}>
+            <Slots>
+              {(_, { x, y, step }) => (
+                <circle
+                  key={step}
+                  cx={x}
+                  cy={y}
+                  r={5}
+                  className="fill-slate-500 stroke-transparent"
+                />
+              )}
+            </Slots>
+
+            {children}
           </g>
-        ))}
+        </svg>
 
-        <g transform={`translate(${size / 2} ${size / 2})`}>
-          <Slots>
-            {(_, { x, y, step }) => (
-              <circle
-                key={step}
-                cx={x}
-                cy={y}
-                r={5}
-                className="fill-slate-500 stroke-transparent"
-              />
-            )}
-          </Slots>
-
-          {children}
-        </g>
-      </svg>
+        <DimensionLabels
+          dimensions={dimensions}
+          diagramWidth={size}
+          range={range}
+        />
+      </div>
     </RadarContext.Provider>
   )
 }
@@ -97,5 +112,139 @@ const Circles = ({ steps }: CircleProps) => {
         />
       ))}
     </>
+  )
+}
+
+type DimensionLabelsProps = {
+  diagramWidth: number
+  range: Range
+  dimensions: string[]
+}
+
+const DimensionLabels = ({
+  dimensions,
+  diagramWidth,
+  range,
+}: DimensionLabelsProps) => {
+  const [, max] = range
+
+  return (
+    <div
+      style={{
+        position: "absolute",
+        left: diagramWidth / 2,
+        top: diagramWidth / 2,
+      }}
+    >
+      {dimensions.map((dimension, index) => {
+        const { x, y } = getPoint({
+          diagramWidth,
+          range,
+          value: max,
+          angle: getDimensionAngle(dimensions, index),
+        })
+
+        return (
+          <div
+            key={dimension}
+            style={{ position: "absolute", top: y, left: x }}
+          >
+            <Text align={getAlign(x)} justify={getJustify(y)}>
+              {dimension}
+            </Text>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+const enum Align {
+  left = "left",
+  center = "center",
+  right = "right",
+}
+
+const enum Justify {
+  top = "top",
+  center = "center",
+  bottom = "bottom",
+}
+
+const getAlign = (x: number): Align => {
+  if (x < 0) {
+    return Align.right
+  }
+
+  if (x > 0) {
+    return Align.left
+  }
+
+  return Align.center
+}
+
+const getJustify = (y: number): Justify => {
+  if (y < 0) {
+    return Justify.bottom
+  }
+
+  if (y > 0) {
+    return Justify.top
+  }
+
+  return Justify.center
+}
+
+type TextProps = {
+  children: string
+  align: Align
+  justify: Justify
+}
+
+const Text = ({ children, align, justify }: TextProps) => {
+  const ref = useRef<HTMLSpanElement | null>(null)
+
+  const [left, setLeft] = useState(0)
+  const [top, setTop] = useState(0)
+
+  useLayoutEffect(() => {
+    const node = ref.current
+
+    if (!node) {
+      return
+    }
+
+    const { width, height } = node.getBoundingClientRect()
+    const padding = 10
+
+    if (align === Align.left) {
+      setLeft(padding)
+    }
+
+    if (align === Align.right) {
+      setLeft(-(width + padding))
+    }
+
+    if (align === Align.center) {
+      setLeft(-(width / 2))
+    }
+
+    if (justify === Justify.top) {
+      setTop(padding)
+    }
+
+    if (justify === Justify.bottom) {
+      setTop(-(height + padding))
+    }
+
+    if (justify === Justify.center) {
+      setTop(-(height / 2))
+    }
+  }, [align, justify])
+
+  return (
+    <span className="relative" ref={ref} style={{ left, top }}>
+      {children}
+    </span>
   )
 }
