3a7d90fbd86d856a986adcd4e4dab41339e137d4
Philipp Giese <187786+frontendphil@users.noreply.github.com>
2022-04-04 21:46:28 +0200

    chore: extend test-utils to better deal with routes

--- src/pages/test-utils.tsx
+++ src/pages/test-utils.tsx
@@ -1,6 +1,6 @@
 import { act } from "react-dom/test-utils"
 import { render as baseRender } from "@testing-library/react"
-import { MemoryRouter } from "react-router-dom"
+import { MemoryRouter, Route, Routes } from "react-router-dom"
 import { MockedProvider, MockedResponse } from "@apollo/client/testing"
 
 const zeroTimeout = () => new Promise<void>((resolve) => setTimeout(resolve, 0))
@@ -13,19 +13,35 @@ export const finishMutations = async (...mutations: unknown[]) => {
   })
 }
 
+export const finishQueries = async (...queries: unknown[]) => {
+  await act(async () => {
+    for (const _ of queries) {
+      await zeroTimeout()
+    }
+  })
+}
+
 type Component = Parameters<typeof baseRender>[0]
 type Options = Parameters<typeof baseRender>[1] & {
   mocks?: MockedResponse[]
+  path?: string
+  route?: string
 }
 
 export const render = (
   component: Component,
-  { mocks, ...options }: Options = {}
+  { mocks, path, route, ...options }: Options = {}
 ): ReturnType<typeof baseRender> => {
   return baseRender(
-    <MemoryRouter>
+    <MemoryRouter initialEntries={route ? [{ pathname: route }] : undefined}>
       <MockedProvider mocks={mocks} addTypename={false}>
-        {component}
+        {path ? (
+          <Routes>
+            <Route path={path} element={component} />
+          </Routes>
+        ) : (
+          component
+        )}
       </MockedProvider>
     </MemoryRouter>,
     options
