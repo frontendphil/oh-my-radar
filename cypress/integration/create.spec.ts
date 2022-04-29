a4a1b40217dfc8b012c3455fd61363b91f0efdaf
Philipp Giese <187786+frontendphil@users.noreply.github.com>
2022-04-29 12:18:54 +0200

    chore: update e2e tests

--- cypress/integration/create.spec.ts
+++ cypress/integration/create.spec.ts
@@ -2,9 +2,7 @@ describe("Create", () => {
   it("is possible to create a new chart.", () => {
     cy.visit("/")
 
-    cy.findByRole("button", { name: "Create new chart" }).click()
-
-    cy.findByRole("figure", { name: "New chart" }).should("exist")
+    cy.findByRole("button", { name: "Create new chart" }).should("exist")
 
     cy.percySnapshot()
   })
