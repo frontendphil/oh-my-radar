bb470f359c03213d50b5a633833a6d39746f5c69
Philipp Giese <187786+frontendphil@users.noreply.github.com>
2022-04-29 13:19:59 +0200

    chore: add two more e2e tests

---
+++ cypress/integration/participate.spec.ts
@@ -0,0 +1,24 @@
+import invariant from "invariant"
+
+describe("Participate", () => {
+  it("is possible to participate in a chart.", () => {
+    cy.visit("/")
+
+    cy.findByRole("button", { name: "Create new chart" }).click()
+
+    cy.waitFor("network")
+
+    cy.findByRole("textbox", { name: "Participant view" })
+      .invoke("val")
+      .then((url) => {
+        invariant(
+          typeof url === "string",
+          `${url} was supposed to be a "string".`
+        )
+
+        cy.visit(url)
+
+        cy.percySnapshot()
+      })
+  })
+})
