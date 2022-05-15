7141e8a1b8980da462928c304e3b2a28905c4dd0
Philipp Giese <187786+frontendphil@users.noreply.github.com>
2022-05-15 19:24:08 +0200

    chore: some styling for the hint

--- src/layout/Hint.tsx
+++ src/layout/Hint.tsx
@@ -4,4 +4,6 @@ type Props = {
   children: ReactNode
 }
 
-export const Hint = ({ children }: Props) => <div>{children}</div>
+export const Hint = ({ children }: Props) => (
+  <div className="flex justify-center p-12 italic opacity-50">{children}</div>
+)
