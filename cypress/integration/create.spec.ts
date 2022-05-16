08f2f400125b7f5f497e34ef1bb62cf26013fcd5
Philipp Giese <187786+frontendphil@users.noreply.github.com>
2022-05-16 21:07:18 +0200

    chore: adjust e2e test

--- cypress/integration/create.spec.ts
+++ cypress/integration/create.spec.ts
@@ -13,15 +13,15 @@ describe("Create", () => {
     cy.waitFor("network")
 
     cy.findByRole("figure", { name: "One" }).should("exist")
-    cy.findByRole("presentation", { name: "One - 1" }).should("exist")
-    cy.findByRole("presentation", { name: "One - 4" }).should("exist")
+    cy.findByRole("radio", { name: "One - 1" }).should("exist")
+    cy.findByRole("radio", { name: "One - 4" }).should("exist")
 
     cy.findByRole("figure", { name: "Two" }).should("exist")
-    cy.findByRole("presentation", { name: "Two - 1" }).should("exist")
-    cy.findByRole("presentation", { name: "Two - 4" }).should("exist")
+    cy.findByRole("radio", { name: "Two - 1" }).should("exist")
+    cy.findByRole("radio", { name: "Two - 4" }).should("exist")
 
     cy.findByRole("figure", { name: "Three" }).should("exist")
-    cy.findByRole("presentation", { name: "Three - 1" }).should("exist")
-    cy.findByRole("presentation", { name: "Three - 4" }).should("exist")
+    cy.findByRole("radio", { name: "Three - 1" }).should("exist")
+    cy.findByRole("radio", { name: "Three - 4" }).should("exist")
   })
 })
