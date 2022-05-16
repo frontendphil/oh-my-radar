19fc0c9a7b4f2005ad21ffd726e80002aa2b76b8
Philipp Giese <187786+frontendphil@users.noreply.github.com>
2022-05-16 17:25:49 +0200

    chore: extract demo component

--- src/pages/create/Create.tsx
+++ src/pages/create/Create.tsx
@@ -1,10 +1,9 @@
 import invariant from "invariant"
-import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
 import { PrimaryButton } from "../../form-controls"
 import { View } from "../../layout"
-import { Colors, RadarChart, Selection } from "../../radar-chart"
 import { useCreateChartMutation, useCreateDimensionsMutation } from "./api"
+import { Demo } from "./Demo"
 
 export const Create = () => {
   const navigate = useNavigate()
@@ -63,49 +62,3 @@ export const defaultDimensions = (chartId: string) => [
 ]
 
 export const defaultChart = () => ({ title: "New chart", min: 1, max: 4 })
-
-const useRandomSelection = () => {
-  const [selection, setSelection] = useState({})
-
-  useEffect(() => {
-    const randomizeSelection = () => {
-      setSelection({
-        acceleration: Math.floor(Math.random() * 5 + 1),
-        displacement: Math.floor(Math.random() * 5 + 1),
-        horsepower: Math.floor(Math.random() * 5 + 1),
-        mpg: Math.floor(Math.random() * 5 + 1),
-        weight: Math.floor(Math.random() * 5 + 1),
-      })
-    }
-
-    randomizeSelection()
-
-    const interval = setInterval(randomizeSelection, 5000)
-
-    return () => clearInterval(interval)
-  }, [])
-
-  return selection
-}
-
-const Demo = () => {
-  const john = useRandomSelection()
-  const jane = useRandomSelection()
-
-  return (
-    <RadarChart
-      title="Comparing Cars Across Dimensions"
-      range={[1, 5]}
-      dimensions={[
-        { id: "acceleration", title: "Acceleration" },
-        { id: "displacement", title: "Displacement" },
-        { id: "horsepower", title: "Horsepower" },
-        { id: "mpg", title: "MPG" },
-        { id: "weight", title: "Weight" },
-      ]}
-    >
-      <Selection name="Jane" value={jane} />
-      <Selection name="John" color={Colors.green} value={john} />
-    </RadarChart>
-  )
-}
