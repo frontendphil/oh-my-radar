e7c0aa6415b066395b702afd1be0717c01ae2ad7
Philipp Giese <187786+frontendphil@users.noreply.github.com>
2022-05-18 10:42:31 +0200

    chore: extract eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,3 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: ["plugin:react/recommended", "plugin:react-hooks/recommended"],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-  plugins: ["react", "@typescript-eslint"],
-
-  rules: {
-    "react/react-in-jsx-scope": "off",
-  },
+  extends: "@radar",
 }
