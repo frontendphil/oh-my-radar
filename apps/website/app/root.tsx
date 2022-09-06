10f61f2996342fc79f7e714c39586e4f7363d56a
Philipp Giese <187786+frontendphil@users.noreply.github.com>
2022-09-06 11:14:38 +0200

    chore: change site title

--- apps/website/app/root.tsx
+++ apps/website/app/root.tsx
@@ -13,7 +13,7 @@ import styles from "./styles/app.css"
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "Oh My Radar 📡",
   viewport: "width=device-width,initial-scale=1",
 })
 
