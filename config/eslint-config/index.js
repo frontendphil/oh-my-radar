73b97e11549816a81f1c2d40383561150d56612b
Philipp Giese <187786+frontendphil@users.noreply.github.com>
2022-09-28 21:16:23 +0200

    chore: add more eslint configuration and lint task (#350)
    * chore: add more eslint configuration and lint task
    
    * chore: update turbo config
    
    * chore: make eslint parse all files
    
    * chore: run eslint before prettier
    
    * chore: fix config error

--- config/eslint-config/index.js
+++ config/eslint-config/index.js
@@ -3,7 +3,14 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["plugin:react/recommended", "plugin:react-hooks/recommended"],
+  extends: [
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+  ],
+
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
@@ -17,4 +24,12 @@ module.exports = {
   rules: {
     "react/react-in-jsx-scope": "off",
   },
+
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+
+  overrides: [{ files: ["*.ts", "*.tsx", "*.js"] }],
 }
